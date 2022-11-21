516450b2fa43a18cc265d1f7415ce0686e96143d
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-21 15:19:48 +0300

    example009

---
+++ example009_introArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// // Найти из 9 чисел максимальное значение (используем массив)
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {13, 25, 13, 324, 55, 6, 77, 83, 19}; // массив
+
+int rez = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])); 
+
+Console.WriteLine(rez);
